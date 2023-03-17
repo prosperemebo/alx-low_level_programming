6c9eb78103993f1aca38ddd27c8007dea9af578f
prosperemebo <emebopropser@gmail.com>
2023-03-16 17:54:39 -0700

    new Push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+  * main - Entry point
+  * prints simple message
+  *
+  * Return: Always 0 (success)
+*/
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
