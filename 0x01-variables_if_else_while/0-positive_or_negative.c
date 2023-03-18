437c191c0f4d32cc67e56dc94a1241564c319aa8
Prosper Emebo <emeboprosper@gmail.com>
2023-03-18 18:30:05 +0100

    New Push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,26 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/*
+* main - Application entry point
+* @int n - Random number
+	Prints if n is positive, negative or 0
+* Return - 0 (Success)
+*/
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
+	if(n > 0)
+		printf("%i is positive\n", n);
+	else if(n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%1 is zero\n", n);
+
 	return (0);
 }
