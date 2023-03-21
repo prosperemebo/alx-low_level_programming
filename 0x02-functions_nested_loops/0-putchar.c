a827a5d34eba193bff6da835b990eda0e127fca3
Prosper Emebo <emeboprosper@gmail.com>
2023-03-21 17:50:00 +0100

    new Push

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+* main - Entry point for program
+* 
+* Returns - 0 on success
+*/
+
+int main() {
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+## 0x02-functions_nested_loops
+This project is concerned with nested loops
