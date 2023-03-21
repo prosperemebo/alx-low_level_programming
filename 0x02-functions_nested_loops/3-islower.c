551ed064bc7d9620909a6e3bf1c9994d7b5d7afa
Prosper Emebo <emeboprosper@gmail.com>
2023-03-21 18:06:23 +0100

    new push

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character
+* @c: character to be checked
+*
+* Returns: 0 or 1
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
