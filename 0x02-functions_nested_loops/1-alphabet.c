99e40ad574d319f24c59df2005554f81197cdd80
Prosper Emebo <emeboprosper@gmail.com>
2023-03-21 18:03:51 +0100

    new push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints characters
+ *
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	putchar('\n');
+}
