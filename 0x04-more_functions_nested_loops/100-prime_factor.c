7468a983b47e6dfd25a8bbb2714e207ef7f1278a
Prosper Emebo <emeboprosper@gmail.com>
2023-03-23 18:22:34 +0100

    New Commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,12 +11,11 @@ int main(void)
 	long int num = 612852475143;
 	long int largest_prime_factor = 2;
 
-	while (num > largest_prime_factor) {
+	while (num > largest_prime_factor)
 		if (num % largest_prime_factor == 0)
 			num /= largest_prime_factor;
 		else
 			largest_prime_factor++;
-	}
 
 	printf("%ld\n", largest_prime_factor);
 	return (0);
