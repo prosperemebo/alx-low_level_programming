dc47a0be9fff202d80779fdfb13398a63d5472f7
Prosper Emebo <emeboprosper@gmail.com>
2023-03-23 14:25:51 +0100

    New push

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,7 +10,7 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 9:; i++)
+	for (i = 0; i < 9; i++)
 		_putchar('0' + i);
 	_putchar('\n');
 }
