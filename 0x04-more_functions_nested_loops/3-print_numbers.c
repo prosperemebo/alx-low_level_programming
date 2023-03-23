c76800f554f92469f3febade3e32726c483f78be
Prosper Emebo <emeboprosper@gmail.com>
2023-03-23 14:23:25 +0100

    new Push

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
  * Return: - (0) on sucess
  */
 
-void print_numbers (void)
+void print_numbers(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 		_putchar(i);
 	_putchar('\n');
 }
