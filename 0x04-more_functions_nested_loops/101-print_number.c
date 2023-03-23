ff9191a40ae25a33afb171477e469d1434972d2d
Prosper Emebo <emeboprosper@gmail.com>
2023-03-24 00:31:17 +0100

    new Push

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,13 +11,14 @@
 
 void print_number(int n)
 {
-	unsigned int number = n;
+	unsigned int new_n = n;
 
 	if (n < 0)
+	{
 		_putchar('-');
-		number = -number;
-
-	if ((number / 10) > 0)
-		print_number(number / 10);
-	_putchar((number % 10) + '0');
+		new_n = -new_n;
+	}
+	if ((new_n / 10) > 0)
+		print_number(new_n / 10);
+	_putchar((new_n % 10) + '0');
 }
