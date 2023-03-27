e812755c9a5b9cf67f9fc33d320e795840899b03
Prosper Emebo <emeboprosper@gmail.com>
2023-03-27 23:37:09 +0100

    New Push

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - checks string length
+ * @s: string argument
+ *
+ * Return: length of @s
+ */
+
+int _strlen(char *s)
+{
+	int x = strlen(s);
+	return x;
+}
