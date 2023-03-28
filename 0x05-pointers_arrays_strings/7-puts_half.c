aafa6517e3e6b9f152b5577fc966d094c3aa926e
Prosper Emebo <emeboprosper@gmail.com>
2023-03-28 03:49:44 +0100

    new push

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half -  half of a string, followed by a new line.
+ * @str: string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (j = (i + 1) / 2; str[j] != '\0'; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
