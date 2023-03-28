00994e8db707ba2269e68f5bd117e5c20d06674c
Prosper Emebo <emeboprosper@gmail.com>
2023-03-28 03:27:50 +0100

    new Push

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to a console
+ * @str: char
+ * 
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
