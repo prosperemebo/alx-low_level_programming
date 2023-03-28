dea80dea6b7d71a1a61c8b4a5824888674d871ef
Prosper Emebo <emeboprosper@gmail.com>
2023-03-28 03:47:57 +0100

    new push

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other string, followed by a new line, to stdout
+ * @str: string
+ *
+ * Return: Void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
