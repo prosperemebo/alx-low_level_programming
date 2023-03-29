e49b578fca4cbc4d6a202795fab53b7bd79d8bed
Prosper Emebo <emeboprosper@gmail.com>
2023-03-30 00:50:04 +0100

    New Push

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: Source String
+ * @dest: Destnation String
+ * @n: Number of bytes
+ *
+ * Return: destination String
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	dest[i] = ' ';
+
+	for (j = 0; src[j] != '\0' && j < n; i++, j++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
