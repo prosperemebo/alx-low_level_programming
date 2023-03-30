071291bf271f50b1383abf538bbce40153b6726a
Prosper Emebo <emeboprosper@gmail.com>
2023-03-30 02:30:42 +0100

    new push

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @s: Input String
+ *
+ * Return: string
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL",
+	     numbers[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+				s[i] = numbers[j];
+		}
+	}
+
+	return (s);
+}
