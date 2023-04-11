47b84cefd71d7122f78d22337a0f9ab70907c3bc
Prosper Emebo <emeboprosper@gmail.com>
2023-04-11 10:54:08 +0100

    New Push

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program entry point
+ *
+ * @argc: int
+ * @argv: vector
+ * Return: (0) on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, coin;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (coin = 0; n > 0; coin++)
+	{
+		if (n >= 25)
+			n -= 25;
+		else if (n >= 10)
+			n -= 10;
+		else if (n >= 5)
+			n -= 5;
+		else if (n >= 2)
+			n -= 2;
+		else
+			n -= 1;
+	}
+
+	printf("%i\n", coin);
+
+	return (0);
+}
