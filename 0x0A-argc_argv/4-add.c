d36a58afdf8921815e1086f808beb2f5585fce77
Prosper Emebo <emeboprosper@gmail.com>
2023-04-11 10:51:53 +0100

    New Push

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program entry point
+ *
+ * @argc: int
+ * @argv: vector
+ * Return: (0) On Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, arg, sum;
+	char *p;
+
+	for (i = 1, sum = 0; i < argc; i++)
+	{
+		arg = strtol(argv[i], &p, 10);
+		if (*p !=  '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += arg;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
