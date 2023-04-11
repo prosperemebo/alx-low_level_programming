eaf60bb4c8c5b746bbfcdf21e42efe56ff6925e6
Prosper Emebo <emeboprosper@gmail.com>
2023-04-12 00:40:48 +0100

    New Push

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory
+ *
+ * @str: string
+ * Return: pointer or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int i, len;
+
+	if (str == NULL)
+		return (NULL);
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	p = (char *)malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		p[i] = str[i];
+	return (p);
+	free(p);
+}
