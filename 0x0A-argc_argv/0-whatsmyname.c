5dd9662e8ee539dd0021e3cb9c6140b9fa2bd08c
Prosper Emebo <emeboprosper@gmail.com>
2023-04-11 10:46:42 +0100

    New Push

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry Point
+ *
+ * @argc: int
+ * @argv: vector
+ * Return: 0 on Succes
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
