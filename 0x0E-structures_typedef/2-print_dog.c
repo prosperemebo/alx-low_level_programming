051436e33e33c8ca8b4516f04ac616d83cac27ad
Prosper Emebo <emeboprosper@gmail.com>
2023-04-17 21:37:09 +0100

    New Push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,11 @@
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
 
 /**
  * print_dog - Prints struct dog
  *
- * @d: Real Dog
+ * @d: Dog
  */
 
 void print_dog(struct dog *d)
@@ -14,10 +14,8 @@ void print_dog(struct dog *d)
 	{
 		if (!d->name)
 			d->name = "(nil)";
-
 		if (!d->owner)
 			d->owner = "(nil)";
-
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
