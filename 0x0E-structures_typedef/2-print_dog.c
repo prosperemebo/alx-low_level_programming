978006f25efdedbc7d25e2847604ca3330a921e4
Prosper Emebo <emeboprosper@gmail.com>
2023-04-17 21:32:42 +0100

    New Push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,13 +10,14 @@
 
 void print_dog(struct dog *d)
 {
-	if (!d->name)
-		d->name = "Undefined!";
+	if (d)
+		if (!d->name)
+			d->name = "(nil)";
 		
-	if (!d->owner)
-		d->owner = "Undefined!";
+		if (!d->owner)
+			d->owner = "(nil)";
 
-	printf("Name: %s\n", d->name);
-	printf("Owner: %s\n", d->owner);
-	printf("Age: %f\n", d->age);
+		printf("Name: %s\n", d->name);
+		printf("Owner: %s\n", d->owner);
+		printf("Age: %f\n", d->age);
 }
