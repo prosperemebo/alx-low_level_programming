0ef9b14d59bef1fd1a2359881af5f15d2fcc0df9
Prosper Emebo <emeboprosper@gmail.com>
2023-04-20 12:09:08 +0100

    New Push

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program entry point
+ *
+ * @ac: arg number
+ * @av: args
+ * Return: (0) on success
+ */
+
+int main(int ac, char *av[])
+{
+	int i, j;
+	char *address;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(av[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	address = (char *)main;
+	for (i = 0; i < atoi(av[1]); i++)
+	{
+		if (i == (atoi(av[1]) - 1))
+		{
+			printf("%.2hhx\n", address[i]);
+			return (0);
+		}
+		printf("%02hhx ", address[i]);
+	}
+
+	return (0);
+}
