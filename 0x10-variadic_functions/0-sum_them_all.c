47c273bd53ef63eea8ad1cca7a0fcfb45a7d481f
Prosper Emebo <emeboprosper@gmail.com>
2023-04-21 01:03:37 +0100

    new push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,19 +5,22 @@
  *
  * @n: First input
  * @...: Other inputs
- * Return: Always 0.
+ * Return: Sum of all inputs
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	int i;
-	int sum = 0;
+    va_list ap;
+    unsigned int i;
+    int sum = 0;
 
-	va_start(ap, n);
-	for (i = n; i >= 0; i = va_arg(ap, int))
-		sum += i;
-	va_end(ap);
+    if (n == 0)
+        return (0);
 
-	return (sum);
+    va_start(ap, n);
+    for (i = 0; i < n; i++)
+        sum = sum + va_arg(ap, int);
+    va_end(ap);
+
+    return (sum);
 }
