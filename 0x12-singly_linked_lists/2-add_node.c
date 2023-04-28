efa9e364f02a114657d66266b8f4683dca18fcc4
Prosper Emebo <emeboprosper@gmail.com>
2023-04-28 02:48:50 +0100

    New Push

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to pointer of first node of list_t list
+ * @str: string to be added to the list_t list
+ *
+ * Return: address of the new element or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (!head || str)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
