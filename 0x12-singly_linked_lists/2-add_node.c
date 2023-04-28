f4b4b8697ad5f195a3fe541f7b8c386d10c50aa3
Prosper Emebo <emeboprosper@gmail.com>
2023-04-28 03:00:15 +0100

    New Push

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,7 +17,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = _strlen(new_node->str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
