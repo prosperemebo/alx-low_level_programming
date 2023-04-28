a0332fc42d6d9016863f8c37480c1b72beebc631
Prosper Emebo <emeboprosper@gmail.com>
2023-04-28 03:22:33 +0100

    New Push

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ *
+ * @head: A pointer to a pointer to the head node of the list.
+ * @str: The string to be stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (!*head)
+		return (new_node);
+
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+
+	return (new_node);
+}
+
+/**
+ * _strlen - Computes the length of a string.
+ *
+ * @str: A pointer to the string to compute the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
