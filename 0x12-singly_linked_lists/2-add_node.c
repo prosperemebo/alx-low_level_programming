c1ad7ceadc4fdc628c21f1c1ad5595d5f68f9c7d
Prosper Emebo <emeboprosper@gmail.com>
2023-04-28 02:55:55 +0100

    New Push

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,20 +12,11 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (!head || str)
-		return (NULL);
-
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (!new_node->str)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
 	new_node->len = _strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
