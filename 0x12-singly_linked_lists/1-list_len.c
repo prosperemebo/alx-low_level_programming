ed7f13e95eca1431b0155e4128b99eeb479a1c0a
Prosper Emebo <emeboprosper@gmail.com>
2023-04-28 02:27:11 +0100

    fix: Added betty style documentation to task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * list_len - counts the number of nodes in a linked list
+ * @h: a pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+
 size_t list_len(const list_t *h)
 {
 	int i;
