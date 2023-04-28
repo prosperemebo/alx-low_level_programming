6edc9bae782a954af04a87924d0fd57d74a3bb71
Prosper Emebo <emeboprosper@gmail.com>
2023-04-28 03:52:02 +0100

    New Push

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,8 +8,8 @@
 
 int __attribute__((constructor)) first()
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 
 	return (0);
 }
