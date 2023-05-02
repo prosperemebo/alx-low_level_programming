1d2ce3c7e1ba1683fbbd19b981b7e316d653e067
Prosper Emebo <emeboprosper@gmail.com>
2023-05-02 16:05:11 +0100

    New Push

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - deletes the node at
+ * index of a listint_t linked list
  * @head: double pointer to the head of the linked list
  * @index: index of the node that should be deleted. Index starts at 0
  *
  * Return: 1 if it succeeded, -1 if it failed
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp, *prev;
