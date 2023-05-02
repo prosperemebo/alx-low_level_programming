fca51e24947389a651646963be3e7579c990d959
Prosper Emebo <emeboprosper@gmail.com>
2023-05-02 16:16:24 +0100

    New Push

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a listint_t list safely
+ * @h: Pointer to a pointer to the head node of the list
+ *
+ * Return: The size of the list that was free'd
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *next;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	current = *h;
+
+	while (current != NULL)
+	{
+		count++;
+		next = current->next;
+		free(current);
+		if (next >= current)
+		{
+			*h = NULL;
+			exit(98);
+		}
+		current = next;
+	}
+
+	*h = NULL;
+
+	return (count);
+}
