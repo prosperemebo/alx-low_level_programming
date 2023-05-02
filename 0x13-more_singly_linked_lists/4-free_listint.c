b588170c176e63983eda7eace6284b939d4c7787
Prosper Emebo <emeboprosper@gmail.com>
2023-05-02 13:43:12 +0100

    New Push

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * free_listint - Frees a linked listint_t list.
+ *
+ * @head: A pointer to the head of the list.
+ *
+ * Return: void.
+ */
+
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
