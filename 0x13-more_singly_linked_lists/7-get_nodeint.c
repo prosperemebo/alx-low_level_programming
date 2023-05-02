63cf1d855d85dd2bc1d3c9d928fa75152fcec6cb
Prosper Emebo <emeboprosper@gmail.com>
2023-05-02 15:05:16 +0100

    New Push

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,14 @@
 #include "lists.h"
 
+/**
+ * get_nodeint_at_index - Returns the nth node of a listint_t linked list.
+ *
+ * @head: Pointer to the first element of the list
+ * @index: Index of the node to be returned
+ *
+ * Return: Pointer to the indexed node or NULL if the node does not exist.
+ */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	size_t i = 0;
