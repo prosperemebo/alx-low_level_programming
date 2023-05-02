30b46df419774dd44ba1feec6b9780d73542ce46
Prosper Emebo <emeboprosper@gmail.com>
2023-05-02 13:57:22 +0100

    New Push

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: Void.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+
+		temp = *head;
+		*head = temp->next;
+
+		free(temp);
+	}
+
+	head = NULL;
+}
