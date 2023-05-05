18b8ae67bd3f2a8f415e4e9c0d3138cd8de7783e
Prosper Emebo <emeboprosper@gmail.com>
2023-05-05 01:28:59 +0100

    New Push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
 	for (i = 0; b[i] != '\0'; i++)
@@ -30,7 +30,7 @@ unsigned int binary_to_uint(const char *b)
 		}
 		else
 		{
-			return 0;
+			return (0);
 		}
 	}
 
