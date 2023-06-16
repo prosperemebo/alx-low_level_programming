2fc16133706b875ca7e91b005be40834543e626b
Prosper Emebo <emeboprosper@gmail.com>
2023-06-16 05:24:28 +0100

    New ush

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,22 +9,22 @@
  *
  * Return: Address of the new element, or NULL if it failed.
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node;
 
-	if (new_node == NULL)
-		return NULL;
-
-	new_node->n = n;
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 	new_node->prev = NULL;
-	new_node->next = *head;
-
-	if (*head != NULL)
+	new_node->n = n;
+	if (!(*head))
+		new_node->next = NULL;
+	else
+	{
+		new_node->next = *head;
 		(*head)->prev = new_node;
-
+	}
 	*head = new_node;
-
 	return (new_node);
 }
