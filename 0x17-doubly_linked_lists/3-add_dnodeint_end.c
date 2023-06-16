ed9ce0830e8ca01c310f5531babbd9ef2780ca47
Prosper Emebo <emeboprosper@gmail.com>
2023-06-16 05:25:12 +0100

    New Push

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,27 +10,24 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *last = *head;
+	dlistint_t *temp, *new_node;
 
-	if (new_node == NULL)
-		return NULL;
-
-	new_node->n = n;
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 	new_node->next = NULL;
-
-	if (*head == NULL)
+	new_node->n = n;
+	if (!(*head))
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return new_node;
 	}
-
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = new_node;
-	new_node->prev = last;
-
+	else
+	{
+		for (temp = *head; temp->next; temp = temp->next)
+			;
+		new_node->prev = temp;
+		temp->next = new_node;
+	}
 	return (new_node);
 }
