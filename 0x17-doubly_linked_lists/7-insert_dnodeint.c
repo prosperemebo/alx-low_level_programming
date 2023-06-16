dbc91705c046879da91a4d02caeb9565e23a5287
Prosper Emebo <emeboprosper@gmail.com>
2023-06-16 05:27:48 +0100

    New push

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,3 +34,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	return (new_node);
 }
+
+/**
+ * dlistint_len - returns number of elements
+ * @h: head
+ * Return: int number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; h = h->next, i++)
+		;
+	return (i);
+}
