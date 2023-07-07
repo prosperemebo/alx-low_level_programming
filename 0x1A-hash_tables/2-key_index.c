751f79b34c21c0ecf9aa087b0a0da188a9d63061
Prosper Emebo <emeboprosper@gmail.com>
2023-07-07 22:20:20 +0100

    New Push

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,10 +2,10 @@
 
 /**
  * key_index - Gives you the index of a key.
- * 
+ *
  * @key: Input key
  * @size: Input size
- * 
+ *
  * Return: Resolved index
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
