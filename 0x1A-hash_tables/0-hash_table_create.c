f402c6d34affe9ac4f9bba0aa4fa2c7c87246356
Prosper Emebo <emeboprosper@gmail.com>
2023-07-07 21:49:38 +0100

    New Push

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table, or NULL if an error occurs.
+ */
+hash_table_t *hash_table_create(unsigned long int size) {
+	hash_table_t *hash_table = NULL;
+
+	if (size == 0)
+		return NULL;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return NULL;
+
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return NULL;
+	}
+
+	return hash_table;	
+}
