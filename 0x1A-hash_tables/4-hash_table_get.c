45d92757b26143979a342a0f7b82173683930e5a
Prosper Emebo <emeboprosper@gmail.com>
2023-07-07 22:15:06 +0100

    New Push

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves value from input hash table.
+ * 
+ * @ht: Input hash table
+ * @key: Key
+ * 
+ * Return: Retrived value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index;
+	hash_node_t *head;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] && !strcmp(ht->array[index]->key, key))
+		return (ht->array[index]->value);
+	for (head = ht->array[index]; head; head = head->next)
+	{
+		if (!strcmp(head->key, key))
+			return (head->value);
+	}
+	return (NULL);
+}
