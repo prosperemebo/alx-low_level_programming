5da86c6d9fc8c1c4d6a38e9d885b96474ce130c7
Prosper Emebo <emeboprosper@gmail.com>
2023-07-07 22:23:30 +0100

    New Push

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes input hash table.
+ *
+ * @ht: hash table
+ *
+ * Return: Null on success
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	int i;
+
+	for (i = 0; i < (int)ht->size; i++)
+	{
+		if (ht->array[i])
+			free_list(ht->array[i]);
+	}
+	free(ht->array);
+	free(ht);
+}
+/**
+ * free_list - Frees input list
+ *
+ * @array: list
+ *
+ * Returns: NULL on success
+ */
+void free_list(hash_node_t *array)
+{
+	hash_node_t *head = array;
+
+	while (array)
+	{
+		head = array;
+		free(array->key);
+		free(array->value);
+		free(head);
+		array = array->next;
+	}
+}
