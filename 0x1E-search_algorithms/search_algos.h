76add2b7f95b0d59cec8a88239f1ca173c257f82
prosperemebo <emeboprosper@gmail.com>
2023-10-12 02:01:04 +0100

    New Push

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -46,5 +46,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_helper(int *array, int min, int max, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int interpolation_helper(int *array, int min, int max, int value);
 
 #endif
